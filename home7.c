e94eecb71318db42d8c4fd6e6a88c587a18ba349
fatima742 <fatimaalamin903@gmail.com>
2025-01-10 03:51:21 +0200

    done

--- home7.c
+++ home7.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-void string(char *str){
-    
-    for (int i = 0; i < 1; i++)
-    {
-        printf("%s\n",str);
-    }
-    
+void string(char *str)
+{
+    printf("%s",str);
+   
 }
 int main ()
 {
-    char str [7]="fatima";
+    char str []="fatima";
     char *s = str;
     string(s);
     return(0);
