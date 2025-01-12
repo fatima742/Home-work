58aeacca19062bdde2eb90ca2c0f3eb29585d272
fatima742 <fatimaalamin903@gmail.com>
2025-01-12 09:31:14 +0200

    done

---
+++ home9.py
@@ -0,0 +1,10 @@
+i=[2, 6, -1, 0, -5]
+for x in i:
+    if(x > 0):
+        print("positiv",x)
+        print(x-2)
+    elif(x < 0):
+        print("Negaitiv",x)
+        print(x+2)
+    else:
+                print("zERO")
