8a5242e362e5dde1c2d9b19fd0f3fe81a886fa74
fatima742 <fatimaalamin903@gmail.com>
2025-01-15 20:08:43 +0200

    done

---
+++ home10.py
@@ -0,0 +1,16 @@
+x = 0
+while x < 7:
+    if x == 0:
+        print("  1",end="")
+    if x == 1:
+        print(" 111",end="")
+    if x == 2:
+        print("11111")
+    if x == 3:
+        print("22222",end="")
+    if x == 4:
+        print(" 222",end="")
+    if x == 5:
+        print("  2",end="")
+    print()
+    x = x + 1
